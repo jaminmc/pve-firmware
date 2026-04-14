jaminmc/pve-firmware

commit 6b14058d5e07a171276fd9acb88771f86e87efdc

make upload: upload to all our known host architectures
for now this is a shared package, so upload it to all archs.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

Makefile
@@ -74,7 +74,7 @@ submodule dvb-firmware.git/README linux-firmware.git/WHENCE:
 .PHONY: upload
 upload: UPLOAD_DIST ?= $(DEB_DISTRIBUTION)
 upload: $(DEBS)
-	tar cf - $(DEBS) | ssh repoman@repo.proxmox.com -- upload --product pve,pmg,pbs,pdm --dist $(UPLOAD_DIST)
+	tar cf - $(DEBS) | ssh repoman@repo.proxmox.com -- upload --product pve,pmg,pbs,pdm --dist $(UPLOAD_DIST) --arch amd64,arm64
 
 .PHONY: clean
 clean:

commit 251fa1af350081dcaddc0f9fa1b3d5eb64bf3303

make upload: fix uploading to all architectures
We currently have no built-in support to upload to multiple
architectures at the same time, so upload separately; will be
deduplicated anyway.

Fixes: 6b14058 ("make upload: upload to all our known host architectures")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

Makefile
@@ -75,7 +75,8 @@ submodule dvb-firmware.git/README linux-firmware.git/WHENCE:
 .PHONY: upload
 upload: UPLOAD_DIST ?= $(DEB_DISTRIBUTION)
 upload: $(DEBS)
-	tar cf - $(DEBS) | ssh repoman@repo.proxmox.com -- upload --product pve,pmg,pbs,pdm --dist $(UPLOAD_DIST) --arch amd64,arm64
+	tar cf - $(DEBS) | ssh repoman@repo.proxmox.com -- upload --product pve,pmg,pbs,pdm --dist $(UPLOAD_DIST) --arch amd64
+	tar cf - $(DEBS) | ssh repoman@repo.proxmox.com -- upload --product pve,pmg,pbs,pdm --dist $(UPLOAD_DIST) --arch arm64
 
 .PHONY: clean
 clean: